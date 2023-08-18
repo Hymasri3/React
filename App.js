e9502c412eab0de21a72e9e5a3f23b05e0e5bd19
hymasri <hymasribonam3@gmail.com>
2023-08-18 13:55:16 +0530

    solution

---
+++ App.js
@@ -0,0 +1,3 @@
+const ele=React.createElement("div",{},React.createElement("div",{},React.createElement("div",{},React.createElement("h1",{},"hello"))));
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(ele);
